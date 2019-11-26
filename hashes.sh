a2750cc74e783e76eab2d01e1230934e9151355a
Tahina Ramananandro <taramana@microsoft.com>
2019-11-25 22:51:57 -0800

    use F*/KReMLin taramana_load_be_i, QD taramana_integers

--- hashes.sh
+++ hashes.sh
@@ -2,13 +2,13 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=84e00e2ed0c0e533bd37a029705f867a5c8b97b0
 branches[mitls-fstar]=master
-hashes[quackyducky]=e7f62911450b9bfc8fe7bea8a10079a6d3004d4d
-branches[quackyducky]=master
-hashes[FStar]=908367fe9bec434ae6c9f6d80e38ce8b0609f2de
-branches[FStar]=master
+hashes[quackyducky]=0ede743d81f3054c1f8a2e361b4421f28bbd7fae
+branches[quackyducky]=taramana_integers
+hashes[FStar]=482d8be0bdf0710a1f0f50fe9bd7b2497bf720fd
+branches[FStar]=taramana_load_be_i
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
 hashes[hacl-star]=3d3a8bcbd149a9e99ff8e87efeeb142226a6fa32
 branches[hacl-star]=fstar-master
-hashes[kremlin]=d51b905ac0b1ae83c4ba7f0a13e0c94504b67c10
-branches[kremlin]=master
+hashes[kremlin]=2fb4cce4aedb842d208709478b1057d9051a7b9e
+branches[kremlin]=taramana_load_be_i
