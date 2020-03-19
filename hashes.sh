39b968603bb27f4752b13455fc75488106c8ca8f
Guido Martínez <mtzguido@gmail.com>
2020-03-19 12:05:52 -0300

    bump quackyducky too

--- hashes.sh
+++ hashes.sh
@@ -2,8 +2,8 @@ declare -A hashes
 declare -A branches
 hashes[mitls-fstar]=35a1a89e31f9f74d44f9aebbae46cbef42e76ed3
 branches[mitls-fstar]=guido_1905
-hashes[quackyducky]=bac7e553bc2042372f31a01701fa9ccd1fa49998
-branches[quackyducky]=master
+hashes[quackyducky]=6ec09cb806daa5456ab1bef3f0e5c7ba601fc00e
+branches[quackyducky]=guido_1905
 hashes[FStar]=ed143a7cc531c0537b3c64eb6ec82cc62545580b
 branches[FStar]=guido_1905
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
