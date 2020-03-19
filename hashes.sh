d2259bcaa2ec1da223cf9c384cde4035ef85eb43
Guido Martínez <mtzguido@gmail.com>
2020-03-19 02:56:58 -0300

    test hashes for 1905

--- hashes.sh
+++ hashes.sh
@@ -1,14 +1,14 @@
 declare -A hashes
 declare -A branches
-hashes[mitls-fstar]=e86910e2462cc81d5938f871a6ea2ade7916af00
-branches[mitls-fstar]=master
+hashes[mitls-fstar]=35a1a89e31f9f74d44f9aebbae46cbef42e76ed3
+branches[mitls-fstar]=guido_1905
 hashes[quackyducky]=bac7e553bc2042372f31a01701fa9ccd1fa49998
 branches[quackyducky]=master
-hashes[FStar]=fc97abe1a25875c2f4ab7de2954acf0932546491
-branches[FStar]=master
+hashes[FStar]=ed143a7cc531c0537b3c64eb6ec82cc62545580b
+branches[FStar]=guido_1905
 hashes[MLCrypto]=190250bbb8f16e7c3f6a8d443b13600ada4fbe79
 branches[MLCrypto]=master
-hashes[hacl-star]=4cc381720fbd023e71774c7db4c97c6e390ea96f
-branches[hacl-star]=master
+hashes[hacl-star]=626c3169ebd423a36edeae236ecbbc5c1a6224fa
+branches[hacl-star]=guido_1905
 hashes[kremlin]=04054342cb527ecb97633d0d88a739ae0b320146
 branches[kremlin]=master
